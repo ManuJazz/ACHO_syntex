0ebb49f373c4e3391e484090c9bf0e66799cf689
ManuJazz <mjesusaz@alumnos.uenx.es>
2019-11-27 12:58:20 +0100

    Default changes to IntentMessage.session_id

--- action-ManuJazz-Medicine_query-ManuJazz.ACHOSintex.py
+++ action-ManuJazz-Medicine_query-ManuJazz.ACHOSintex.py
@@ -11,9 +11,11 @@ import io
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
+
 class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+        return {section: {option_name: option for option_name, option in self.items(section)} for section in
+                self.sections()}
 
 
 def read_configuration_file(configuration_file):
@@ -25,16 +27,15 @@ def read_configuration_file(configuration_file):
     except (IOError, configparser.Error) as e:
         return dict()
 
+
 def subscribe_query_medicine(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
-    query_medicine(hermes, intentMessage, conf)
+    #internal checking...
+    mqttClient.publish_end_session(intentMessage.session_id, 'Ahora mismo no tienes que tomarte ninguna medicina')
 
-def query_medicine(hermes, intentMessage, conf):
-    mqttClient.publish_start_session_notification('default', 'Ahora mismo no tienes que tomarte ninguna medicina', None)
-    mqttClient.publish_end_session('default', "")
 
 if __name__ == "__main__":
     mqtt_opts = MqttOptions()
     with Hermes(mqtt_options=mqtt_opts) as h, Hermes(mqtt_options=mqtt_opts) as mqttClient:
         h.subscribe_intent("ManuJazz:Medicine_query", subscribe_query_medicine) \
-         .start()
+            .start()
